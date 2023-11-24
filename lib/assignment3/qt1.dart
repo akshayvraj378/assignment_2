1dbb63bd7877febf5a50bfa95ff49e0d9d072592
Vijayleo46 <vijaymartin72@gmail.com>
2023-11-24 10:44:51 +0530

    done

--- lib/assignment3/qt1.dart
+++ lib/assignment3/qt1.dart
@@ -24,45 +24,45 @@ class _Qstn1State extends State<Qstn1> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            title: Text("Settings"),
+            title: const Text("Settings"),
             centerTitle: true,
-            backgroundColor: Colors.black26),
+            backgroundColor: Colors.transparent),
         body: ListView(
           children: [
             Padding(
-              padding: EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(15.0),
               child: ListTile(
                 onTap: () {},
-                leading: Icon(Icons.person),
-                title: Padding(
-                  padding: const EdgeInsets.only(left: 16),
+                leading: const Icon(Icons.person),
+                title: const Padding(
+                  padding: EdgeInsets.only(left: 16),
                   child: Text('Account'),
                 ),
-                shape: Border(
+                shape: const Border(
                     top: BorderSide(width: 1, color: Colors.white),
-                    bottom: BorderSide(width: 1, color: Colors.black54),
+                    bottom: BorderSide(width: 1, color: Colors.black38),
                     left: BorderSide(width: 1, color: Colors.black12),
                     right: BorderSide(width: 1, color: Colors.black12)),
               ),
             ),
             Padding(
-              padding: EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(15.0),
               child: ListTile(
                 onTap: () {},
-                leading: Icon(Icons.privacy_tip),
-                title: Padding(
-                  padding: const EdgeInsets.only(left: 16),
+                leading: const Icon(Icons.privacy_tip),
+                title: const Padding(
+                  padding: EdgeInsets.only(left: 16),
                   child: Text('Privacy Policy'),
                 ),
-                shape: Border(
+                shape: const Border(
                     top: BorderSide(width: 1, color: Colors.white),
-                    bottom: BorderSide(width: 1, color: Colors.black54),
+                    bottom: BorderSide(width: 1, color: Colors.black38),
                     left: BorderSide(width: 1, color: Colors.black12),
                     right: BorderSide(width: 1, color: Colors.black12)),
               ),
             ),
             Padding(
-              padding: EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(15.0),
               child: ListTile(
                 onTap: () {},
                 trailing: Switch(
@@ -74,117 +74,110 @@ class _Qstn1State extends State<Qstn1> {
                     });
                   },
                 ),
-                leading: Icon(Icons.notifications),
-                title: Padding(
-                  padding: const EdgeInsets.only(left: 16),
+                leading: const Icon(Icons.notifications),
+                title: const Padding(
+                  padding: EdgeInsets.only(left: 16),
                   child: Text('Notifications'),
                 ),
-                shape: Border(
+                shape: const Border(
                     top: BorderSide(width: 1, color: Colors.white),
-                    bottom: BorderSide(width: 1, color: Colors.black54),
+                    bottom: BorderSide(width: 1, color: Colors.black38),
                     left: BorderSide(width: 1, color: Colors.black12),
                     right: BorderSide(width: 1, color: Colors.black12)),
               ),
             ),
             Padding(
-              padding: EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(15.0),
               child: ListTile(
                 onTap: () {},
-                leading: Icon(Icons.sunny),
-                title: Padding(
-                  padding: const EdgeInsets.only(left: 16),
+                leading: const Icon(Icons.sunny),
+                title: const Padding(
+                  padding: EdgeInsets.only(left: 16),
                   child: Text('Dark/Light'),
                 ),
-                shape: Border(
+                shape: const Border(
                     top: BorderSide(width: 1, color: Colors.white),
-                    bottom: BorderSide(width: 1, color: Colors.black54),
+                    bottom: BorderSide(width: 1, color: Colors.black38),
                     left: BorderSide(width: 1, color: Colors.black12),
                     right: BorderSide(width: 1, color: Colors.black12)),
               ),
             ),
             Padding(
-              padding: EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(15.0),
               child: ListTile(
                 onTap: () {},
                 trailing: DropdownButton(
                   borderRadius: BorderRadius.circular(27),
-                  // Initial Value
                   value: dropdownvalue,
-
-                  // Down Arrow Icon
                   icon: const Icon(Icons.keyboard_arrow_down),
-
-                  // Array list of items
                   items: items.map((String items) {
                     return DropdownMenuItem(
                       value: items,
                       child: Text(items),
                     );
                   }).toList(),
-                  // After selecting the desired option,it will
-                  // change button value to selected value
                   onChanged: (String? newValue) {
                     setState(() {
                       dropdownvalue = newValue!;
                     });
                   },
                 ),
-                leading: Icon(Icons.language),
-                title: Padding(
-                  padding: const EdgeInsets.only(left: 16),
+                leading: const Icon(Icons.language),
+                title: const Padding(
+                  padding: EdgeInsets.only(left: 16),
                   child: Text('Languange'),
                 ),
-                shape: Border(
+                shape: const Border(
                     top: BorderSide(width: 1, color: Colors.white),
-                    bottom: BorderSide(width: 1, color: Colors.black54),
+                    bottom: BorderSide(width: 1, color: Colors.black38),
                     left: BorderSide(width: 1, color: Colors.black12),
                     right: BorderSide(width: 1, color: Colors.black12)),
               ),
             ),
             Padding(
-              padding: EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(15.0),
               child: ListTile(
                 onTap: () {},
-                leading: Icon(Icons.password),
-                title: Padding(
-                  padding: const EdgeInsets.only(left: 16),
+                leading: const Icon(Icons.password),
+                title: const Padding(
+                  padding: EdgeInsets.only(left: 16),
                   child: Text('Change Password'),
                 ),
-                shape: Border(
+                shape: const Border(
                     top: BorderSide(width: 1, color: Colors.white),
-                    bottom: BorderSide(width: 1, color: Colors.black54),
+                    bottom: BorderSide(width: 1, color: Colors.black38),
                     left: BorderSide(width: 1, color: Colors.black12),
                     right: BorderSide(width: 1, color: Colors.black12)),
               ),
             ),
             Padding(
-              padding: EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(15.0),
               child: ListTile(
                 onTap: () {},
-                leading: Icon(Icons.delete),
-                title: Padding(
-                  padding: const EdgeInsets.only(left: 16),
+                leading: const Icon(Icons.delete),
+                title: const Padding(
+                  padding: EdgeInsets.only(left: 16),
                   child: Text('Delete Account'),
                 ),
-                shape: Border(
+                shape: const Border(
                     top: BorderSide(width: 1, color: Colors.white),
-                    bottom: BorderSide(width: 1, color: Colors.black54),
+                    bottom: BorderSide(width: 1, color: Colors.black38),
                     left: BorderSide(width: 1, color: Colors.black12),
                     right: BorderSide(width: 1, color: Colors.black12)),
               ),
             ),
             Padding(
-              padding: EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(15.0),
               child: ListTile(
                 onTap: () {},
-                leading: Icon(Icons.logout),
-                title: Padding(
-                  padding: const EdgeInsets.only(left: 16),
+                leading: const Icon(Icons.logout),
+                title: const Padding(
+                  padding: EdgeInsets.only(left: 16),
                   child: Text('Log Out'),
                 ),
-                shape: Border(
+                shape: const Border(
                     top: BorderSide(width: 1, color: Colors.white),
-                    bottom: BorderSide(width: 1, color: Colors.black54),
+                    bottom: BorderSide(width: 1, color: Colors.black38),
                     left: BorderSide(width: 1, color: Colors.black12),
                     right: BorderSide(width: 1, color: Colors.black12)),
               ),
